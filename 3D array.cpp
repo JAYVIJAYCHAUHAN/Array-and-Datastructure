773d4bec6112e4e336291c0a0edaaa436ce6e6ab
Jayvijay Chauhan <90252471+JAYVIJAYCHAUHAN@users.noreply.github.com>
2022-05-05 17:46:00 +0530

    Update 3D array.cpp

--- 3D array.cpp
+++ 3D array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+ //this is array of matrix implemention 
     int arr[3][3]={{1,2,3,},{4,5,6},{7,8,9,}};
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
